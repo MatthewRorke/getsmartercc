8bcc139f517a1a1f5c07965d5fb6c966979366d8
Matthew Rorke <mrorke@2u.com>
2022-08-17 09:16:22 +0200

    Entry Point tests for Github Actions

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 echo $GITHUB_WORKSPACE
-ln -s $GITHUB_WORKSPACE /code
-cd /code
-ls -la
-/usr/src/app/bin/codeclimate engines:install
-/usr/src/app/bin/codeclimate analyze
+# ln -s $GITHUB_WORKSPACE /code
+# cd /code
+# ls -la
+CODECLIMATE_DOCKER=1 CODECLIMATE_DEBUG=1 CODECLIMATE_CODE=$GITHUB_WORKSPACE engines:install
+CODECLIMATE_DOCKER=1 CODECLIMATE_DEBUG=1 CODECLIMATE_CODE=$GITHUB_WORKSPACE /usr/src/app/bin/codeclimate analyze
