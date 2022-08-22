3ee9c75fe4d6a0d03f2fc4e5faa566f1231fc862
Matthew Rorke <mrorke@2u.com>
2022-08-22 15:26:40 +0200

    Building Docker Container

--- .docker/test.sh
+++ .docker/test.sh
@@ -1,20 +1,21 @@
 #!/bin/sh -l
+#GITHUB_WORKSPACE="/Users/mrorke/Projects/online-campus/blocks/student_appeals/"
 echo "PREPARING"
 echo $GITHUB_WORKSPACE
-cd $GITHUB_WORKSPACE
+#cd $GITHUB_WORKSPACE
 ls -la
 
-ln -s $GITHUB_WORKSPACE /code
+#ln -s $GITHUB_WORKSPACE /code
 
 docker pull codeclimate/codeclimate
 
 docker run \
   --rm \
-  -e CODECLIMATE_DEBUG=1 -e CODECLIMATE_CODE="/code" \
-  --volume /code:/code \
+  -e CODECLIMATE_DEBUG=1 -e CODECLIMATE_CODE="$GITHUB_WORKSPACE" \
+  --volume "$GITHUB_WORKSPACE":/code \
   --volume /var/run/docker.sock:/var/run/docker.sock \
   --volume /tmp/cc:/tmp/cc \
-  codeclimate/codeclimate ls -la && analyze
+  codeclimate/codeclimate analyze
 
 # CODECLIMATE_DOCKER=1 CODECLIMATE_DEBUG=1 CODECLIMATE_CODE=$GITHUB_WORKSPACE codeclimate engines:install
 # CODECLIMATE_DOCKER=1 CODECLIMATE_DEBUG=1 CODECLIMATE_CODE=$GITHUB_WORKSPACE codeclimate analyze
