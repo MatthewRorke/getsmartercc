8ea9320f9a775d1f90ea0372313ae0e3db2fc55c
Matthew Rorke <mrorke@2u.com>
2022-08-22 14:21:18 +0200

    Building Docker Container

--- .docker/test.sh
+++ .docker/test.sh
@@ -9,7 +9,7 @@ docker run \
   --volume "$GITHUB_WORKSPACE":/code \
   --volume /var/run/docker.sock:/var/run/docker.sock \
   --volume /tmp/cc:/tmp/cc \
-  codeclimate/codeclimate help
+  codeclimate/codeclimate analyze
 
 # CODECLIMATE_DOCKER=1 CODECLIMATE_DEBUG=1 CODECLIMATE_CODE=$GITHUB_WORKSPACE codeclimate engines:install
 # CODECLIMATE_DOCKER=1 CODECLIMATE_DEBUG=1 CODECLIMATE_CODE=$GITHUB_WORKSPACE codeclimate analyze
